9321f4b768e366c771e14fc33c5101ce805b9112
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-12-16 15:18:09 +0000

    class3

--- MyApp.java
+++ MyApp.java
@@ -1,15 +1,11 @@
+//継承元のクラスを親クラス、もしくはスーパークラスという
 class User {
-  // String name = "Me!"; //もう初期化する必要がなくなる
   String name;
 
-  User(String name) { //コンストラクタ(クラスと同じ名前で作る)
-    this.name = name; //Userクラスのnameフィールドに設定するため
-  }                   //メソッド内からクラスのフィールドにthisを使ってアクセスさせる
-
-  //引数なしでインスタンス化された場合のコンストラクタ(メソッドのオーバーロード)
-  User() {
-    // this.name = "Me!";
-    this("Me!");
+  //今回は親クラスのコンストラクタに引数があるため、
+  //子クラスのコンストラクタにも同じ引数を渡す
+  User(String name) {
+    this.name = name;
   }
 
   void sayHi() {
@@ -17,13 +13,38 @@ class User {
   }
 }
 
+//継承するクラスを子クラスまたはサブクラスという
+class AdminUser extends User { //extendsで指定したクラスの継承ができる
+                        //継承したことでAdminUserクラスがUserクラスのメソッドや変数を使えるようになる
+  //コンストラクタ
+  AdminUser(String name) {
+  //コンストラクタを作ると自動的に親クラスのコンストラクタが呼ばれる
+  //親クラスのコンストラクタはsuper();という特殊なメソッドで、
+  //親クラスのコンストラクタに引数がなければこれを省略することもできる
+  super(name);
+  }
+
+  void sayHello() {
+    System.out.println("hello! " + this.name);
+  }
+
+  //メソッドのオーバーライド
+  @Override //アノテーション(オーバーライドしていることを明示的に示せるので、
+            //メソッド名を間違えていたり引数を間違えているとエラーにしてくれる)
+  void syHi() {
+    System.out.println("[admin] hi! " + this.name);
+  }
+}
+
 public class MyApp {
   public static void main(String[] args) {
-    User tom;
-    tom = new User("Tom"); //インスタンス化される時に名前を渡す
-    tom = new User(); //インスタンス化される時に名前を渡さない場合
-
+    User tom = new User("tom");
     System.out.println(tom.name);
     tom.sayHi();
+
+    AdminUser bob = new AdminUser("bob");
+    System.out.println(bob.name);
+    bob.sayHi();
+    bob.sayHello();
   }
 }
--- memo.java
+++ memo.java
@@ -693,3 +693,62 @@ java [ファイル名]で実行
     }
   }
   ===========================
+
+-------------------------------------------------
+
+ - クラス③
+ クラスの継承
+
+
+  ===========================
+  //継承元のクラスを親クラス、もしくはスーパークラスという
+  class User {
+    String name;
+
+    //今回は親クラスのコンストラクタに引数があるため、
+    //子クラスのコンストラクタにも同じ引数を渡す
+    User(String name) {
+      this.name = name;
+    }
+
+    void sayHi() {
+      System.out.println("hi! " + this.name);
+    }
+  }
+
+  //継承するクラスを子クラスまたはサブクラスという
+  class AdminUser extends User { //extendsで指定したクラスの継承ができる
+                          //継承したことでAdminUserクラスがUserクラスのメソッドや変数を使えるようになる
+    //コンストラクタ
+    AdminUser(String name) {
+    //コンストラクタを作ると自動的に親クラスのコンストラクタが呼ばれる
+    //親クラスのコンストラクタはsuper();という特殊なメソッドで、
+    //親クラスのコンストラクタに引数がなければこれを省略することもできる
+    super(name);
+    }
+
+    void sayHello() {
+      System.out.println("hello! " + this.name);
+    }
+
+    //メソッドのオーバーライド
+    @Override //アノテーション(オーバーライドしていることを明示的に示せるので、
+              //メソッド名を間違えていたり引数を間違えているとエラーにしてくれる)
+    void syHi() {
+      System.out.println("[admin] hi! " + this.name);
+    }
+  }
+
+  public class MyApp {
+    public static void main(String[] args) {
+      User tom = new User("tom");
+      System.out.println(tom.name);
+      tom.sayHi();
+
+      AdminUser bob = new AdminUser("bob");
+      System.out.println(bob.name);
+      bob.sayHi();
+      bob.sayHello();
+    }
+  }
+  ===========================

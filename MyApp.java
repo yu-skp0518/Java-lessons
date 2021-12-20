b277cb050f6be9bfe8024ff183d5503939fb60c3
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-12-20 14:48:22 +0000

    practice

--- MyApp.java
+++ MyApp.java
@@ -1,3 +1,4 @@
+/*
 //継承元のクラスを親クラス、もしくはスーパークラスという
 class User {
   String name;
@@ -46,5 +47,28 @@ public class MyApp {
     System.out.println(bob.name);
     bob.sayHi();
     bob.sayHello();
+  }
+}
+*/
+
+public class MyApp {
+  public static void main(String[] args) {
+
+
+    int count = 0;
+    for (int i = 1; i <= 10; i++) {
+
+      if (i % 2 == 0) {
+        count += 1;
+        if (i - 10 != 0) {
+          System.out.print(i + ",");
+        } else {
+          System.out.print(i);
+        }
+      }
+    }
+    System.out.println();
+    System.out.println("偶数は合計" + count + "個でした。");
+
   }
 }

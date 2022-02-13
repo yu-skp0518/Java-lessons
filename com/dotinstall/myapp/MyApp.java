b13181f1191bbb45d6174bd45308c24e61363159
yu-skp0518 <reality_0518@yahoo.co.jp>
2022-02-13 15:37:39 +0000

    review

--- com/dotinstall/myapp/MyApp.java
+++ com/dotinstall/myapp/MyApp.java
@@ -1,16 +1,12 @@
-package com.dotinstall.myapp; //ファイル階層をパッケージとしてこのように表す
-import com.dotinstall.myapp.model.User; //別のパッケージからクラスを読み込むための記述
-import com.dotinstall.myapp.model.AdminUser; //別のパッケージからクラスを読み込むための記述
-// import com.dotinstall.myapp.model.*; model以下のファイルを全て読み込みたい場合はこのように書くこともできる
-
 public class MyApp {
+
   public static void main(String[] args) {
 
-    /*com.dotinstall.myapp.model.User*/ User tom = new User("Tom"); //本来であればcom.~と記述しなければいけないが、
-    tom.sayHi();                                                    //ファイル上部のimportで他ファイルへのアクセス権を付与している為、不要
 
-    AdminUser bob = new AdminUser("Bob");
-    bob.sayHello();
-    bob.sayHi();
+    String msg;
+    msg = "hello world again!";
+
+    System.out.println(msg);
   }
+
 }
